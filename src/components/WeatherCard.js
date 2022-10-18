6fe7eef14da4512cfdb62b71fe40093a1438770f
ewj224 <enochjo0623@gmail.com>
2022-10-17 20:51:40 -0400

    finished with project and changes!

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -18,9 +18,9 @@ function WeatherCard({
                 <WeatherIcon weatherType={weatherType}/>
                 <p className="type">{weatherType}</p>
             </div>
-            <p className="temp">{currentTemp} F</p>
-            <p className="high">Highs: {highTemp} F</p>
-            <p className="low">Lows: {lowTemp} F</p>
+            <p className="temp">{currentTemp} °F</p>
+            <p className="high">Highs: {highTemp} °F</p>
+            <p className="low">Lows: {lowTemp} °F</p>
             <div class="circle circle-one" style={{backgroundColor: `rgba(0,0,0,${(windSpeed/100)+.05})`}}></div>
             <div class="circle circle-two" style={{backgroundColor: `rgba(0,0,0,${(humidity/100)+.05})`}}></div>
             <div class="circle circle-three" style={{backgroundColor: `rgba(0,0,0,${(cloudiness/100)+.05})`}}></div>
